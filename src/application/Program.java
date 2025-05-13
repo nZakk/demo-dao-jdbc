3b5c8aedd1222d81415dd65345792b7d99abda95
Isaac Santos <goncalvessantosisaac@gmail.com>
2025-05-13 15:19:39 -0400

    Seller Update Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,8 +41,17 @@ public class Program {
 		
 		Seller seller1 = new Seller(null, "Isaac Green", "isaac@santos.com", new Date(0), 4500.0, new Department(1, null) );
 
-		sellerDao.insert(seller1);
+		//sellerDao.insert(seller1);
 		System.out.println("Inserted! new id = " + seller1.getId());
+		
+		System.out.println("\n=== TEST 5 : Seller Update ===");
+		
+		seller1 =sellerDao.findById(10);
+		seller1.setBaseSalary(10000.0);
+
+		sellerDao.update(seller1);
+		System.out.println(sellerDao.findById(seller1.getId()));
+		System.out.println("Update Comppleted");
 	}
 
 }
